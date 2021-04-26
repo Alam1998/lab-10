01c5e4609297c3e6d72bbbd390e695e8b7cfe8cd
Md. Alam <shaiankhan77@gmail.com>
2021-04-26 16:23:56 -0400

    lab 10 works version one

---
+++ gimmymynumber.py
@@ -0,0 +1,7 @@
+userstring = input("Gimme ma number that is greater than 100...")
+usernum = int(userstring)
+while usernum<100:
+	print(str(usernum) + " is less than 100, Try again, I've got all day...")
+	userstring = input(str(usernum) + " is less than 100, Try again, I've got all day...")
+	usernum = int(userstring)
+print(str(usernum) + " is greater than 100! Finally! Great job!")
